2ab50e30c4e456d3361134d0e5ab331ef1c12f11
Lucian1729 <lordlucifer729@gmail.com>
2023-11-19 10:53:14 -0500

    added metrics to driver

--- driver.py
+++ driver.py
@@ -6,13 +6,14 @@ import time
 import threading
 import random
 import requests
-
+import statistics
 
 bootstrap_servers = "localhost:9092"
 register_topic = "register"
 heartbeat_topic = "heartbeat"
 test_config_topic = "test_config"
 trigger_topic = "trigger"
+metrics_topic= "metrics"
 
 producer = KafkaProducer(
     bootstrap_servers=bootstrap_servers,
@@ -29,14 +30,20 @@ PING_ENDPOINT = f"http://{SERVER_IP}:{SERVER_PORT}/ping"
 
 def send_request_to_server():
     try:
+        start_time = time.perf_counter()
         response = requests.get(PING_ENDPOINT)
+        end_time = time.perf_counter()
+        latency = (end_time - start_time) * 1000
         if response.status_code == 200:
-            print("Request successful. Response:", response.json())
+            pass
         else:
             print(f"Request failed. Status Code: {response.status_code}")
+        return latency
 
     except requests.RequestException as e:
         print(f"Request Exception: {e}")
+        return None
+    
 
 
 def generate_unique_id():
@@ -82,18 +89,100 @@ def consume_test_config():
         print(f"Received test configuration: {test_config_data}")
 
 
-def avalanche_testing(test_id, message_count_per_driver):
-    for _ in range(message_count_per_driver):
-        send_request_to_server()
+def publish_metrics(node_id, test_id, report_id, metrics, test_status, latencies):
+    metrics_message = {
+        "node_id": node_id,
+        "test_id": test_id,
+        "report_id": report_id,
+        "test_status": test_status,
+        "latencies": latencies,
+        "metrics": metrics,
+    }
+    producer.send(metrics_topic, value=metrics_message)
+    print(f"Published metrics: {metrics_message}")
 
 
-def tsunami_testing(test_id, message_count_per_driver, delay_interval):
+def publish_metrics_async(latencies, node_id, test_id, report_id, test_status):
+    def worker():
+        metrics = calculate_aggregate_metrics(latencies)
+        publish_metrics(node_id, test_id, report_id, metrics, test_status, latencies)
+
+    # Create a thread for the non-blocking publish_metrics
+    thread = threading.Thread(target=worker)
+
+    # Start the thread
+    thread.start()
+
+
+def calculate_aggregate_metrics(latencies):
+    if latencies:
+        mean_latency = statistics.mean(latencies)
+        median_latency = statistics.median(latencies)
+        mode_latency = statistics.mode(latencies)
+        min_latency = min(latencies)
+        max_latency = max(latencies)
+
+        return {
+            "mean_latency": mean_latency,
+            "median_latency": median_latency,
+            "mode_latency": mode_latency,
+            "min_latency": min_latency,
+            "max_latency": max_latency,
+            "num_requests": len(latencies)
+        }
+    else:
+        # Return default metrics if no latencies are available
+        return {
+            "mean_latency": None,
+            "median_latency": None,
+            "mode_latency": None,
+            "min_latency": None,
+            "max_latency": None,
+            "num_requests": 0
+        }
+
+
+def avalanche_testing(test_id, message_count_per_driver, node_id):
+    latencies = []
+    last_time = time.perf_counter()
+    report_id = 1
+
     for _ in range(message_count_per_driver):
-        send_request_to_server()
+        curr_time = time.perf_counter()
+        
+        if curr_time - last_time > 0.2:
+            last_time = curr_time
+            publish_metrics_async(latencies, node_id, test_id, report_id, "running")
+            report_id += 1
+
+        latency = send_request_to_server()
+        if latency:
+            latencies.append(latency)
+    publish_metrics_async(latencies, node_id, test_id, report_id, "complete")
+
+
+def tsunami_testing(test_id, message_count_per_driver, delay_interval, node_id):
+    latencies = []
+    last_time = time.perf_counter()
+    report_id = 1
+
+    for _ in range(message_count_per_driver):
+        curr_time = time.perf_counter()
+        
+        if curr_time - last_time > 0.2:
+            last_time = curr_time
+            publish_metrics_async(latencies, node_id, test_id, report_id, "running")
+            report_id += 1
+
+        latency = send_request_to_server()
+        if latency:
+            latencies.append(latency)
         time.sleep(delay_interval)
+    publish_metrics_async(latencies, node_id, test_id, report_id, "complete")
 
 
-def handle_test_config(test_id):
+
+def handle_test_config(test_id, node_id):
     test_config = test_configs.get(test_id)
 
     if test_config:
@@ -102,16 +191,18 @@ def handle_test_config(test_id):
         delay_interval = test_config.get("test_message_delay", 0)
 
         if test_type == "AVALANCHE":
-            avalanche_testing(test_id, message_count_per_driver)
+            avalanche_testing(test_id, message_count_per_driver, node_id)
         elif test_type == "TSUNAMI":
-            tsunami_testing(test_id, message_count_per_driver, delay_interval)
+            tsunami_testing(test_id, message_count_per_driver, delay_interval, node_id)
         else:
             print(f"Invalid test type: {test_type}")
     else:
         print(f"No test configuration found for test ID: {test_id}")
+    
+    test_configs.pop(test_id)
 
 
-def listen_to_trigger():
+def listen_to_trigger(node_id):
     consumer = KafkaConsumer(
         trigger_topic,
         bootstrap_servers=bootstrap_servers,
@@ -123,7 +214,7 @@ def listen_to_trigger():
         print(f"Received trigger: {trigger_data}")
 
         test_id = trigger_data["test_id"]
-        handle_test_config(test_id)
+        handle_test_config(test_id, node_id)
 
 
 def run():
@@ -131,7 +222,7 @@ def run():
     node_id = generate_unique_id()
 
     consumer_thread = threading.Thread(target=consume_test_config)
-    trigger_thread = threading.Thread(target=listen_to_trigger)
+    trigger_thread = threading.Thread(target=listen_to_trigger, args=(node_id,))
 
     consumer_thread.daemon = True
     trigger_thread.daemon = True
